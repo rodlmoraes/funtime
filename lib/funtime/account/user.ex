4f8aa28267e8ff661c78a5ba607506858d7b92cb
rodlmoraes <rodlmoraes@hotmail.com>
2020-11-06 22:21:00 -0300

    add user schema and account context

---
+++ lib/funtime/account/user.ex
@@ -0,0 +1,32 @@
+defmodule Funtime.Account.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :is_active, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :is_active])
+    |> validate_required([:email, :password, :is_active])
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(
+    %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+  ) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset) do
+    changeset
+  end
+end
